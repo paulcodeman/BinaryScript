fc2db1fd9244ea626d25323943ad5a490ac76724
Pavel <d39reg@gmail.com>
2023-06-08 00:30:51 +0200

    Update apinargs.js
    Приведенный код представляет массив _BS_api_2_functions, содержащий различные функции. Вам нужно добавить свой код в соответствующие функции внутри этого массива. Замените комментарий "// Ваш код здесь..." своим собственным кодом, чтобы реализовать требуемую логику для каждой функции.

--- js/apinargs.js
+++ js/apinargs.js
@@ -1,43 +1,42 @@
-var _BS_api_2_functions=[
-	function()// call
-	{
-		
-		
-	}
-	,function()// call
-	{
-
-	}
-	,function()// add
-	{
-		glob_reg[0]+=pop();
-	}
-	,function()// dec
-	{
-		glob_reg[0]-=pop();
-	}
-	,function()// mul
-	{
-		glob_reg[0]*=pop();
-	}
-	,function()// 
-	{
-		
-	}
-	,function()// ret
-	{
-		i=pushret[pushreti--];
-	}
-	,function()// 
-	{
-		
-	}
-	,function()// goto
-	{
-		
-	}
-	,function()// goto
-	{
-		
-	}
+var _BS_api_2_functions = [
+  function() // call
+  {
+    // Ваш код здесь...
+  },
+  function() // call
+  {
+    // Ваш код здесь...
+  },
+  function() // add
+  {
+    glob_reg[0] += pop();
+  },
+  function() // dec
+  {
+    glob_reg[0] -= pop();
+  },
+  function() // mul
+  {
+    glob_reg[0] *= pop();
+  },
+  function() // ...
+  {
+    // Ваш код здесь...
+  },
+  function() // ret
+  {
+    i = pushret[pushreti--];
+  },
+  function() // ...
+  {
+    // Ваш код здесь...
+  },
+  function() // goto
+  {
+    // Ваш код здесь...
+  },
+  function() // goto
+  {
+    // Ваш код здесь...
+  },
 ];
